469aac3f7930188c365b82ba512dfdcec2eaea2e
americanman <snowboardracer7@gmail.com>
2018-07-05 14:56:58 -0400

    Add three books from API call

--- src/Shelf.js
+++ src/Shelf.js
@@ -12,15 +12,23 @@ class Shelf extends React.Component {
 
   componentDidMount() {
     let books = BooksAPI.search('Art');
-    books.then(response => this.setState({books: {'currently_reading': response}}));
+    books.then(response => this.setState({books:
+      {'currently_reading': response, 'want_to_read': response, 'read': response}
+    }));
   }
 
   render() {
     if(Object.keys(this.state.books).length !== 0) {
       let bookArr = [];
+      bookArr.push(<Book key={this.state.books.currently_reading[0].id} book={this.state.books.currently_reading[0]} />)
+      bookArr.push(<Book key={this.state.books.currently_reading[1].id} book={this.state.books.currently_reading[1]} />)
+      bookArr.push(<Book key={this.state.books.currently_reading[2].id} book={this.state.books.currently_reading[2]} />)
+        /*
       this.state.books.currently_reading.map((book,index) =>
-        bookArr.push(<Book key={book.id} book={book} />)
+        bookArr.push(<book key={book.id} book={book} />)
       )
+      */
+      console.log(this.state)
       return(
         <div className="bookshelf-title">
           <h1>{this.props.titleSection}</h1>
